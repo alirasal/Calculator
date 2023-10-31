0815d58b583a3ac6a6ffcdfff6ae35fec647360b
alirasal <allirasll@gmail.com>
2023-10-31 12:46:01 +0530

    Initial Commit

---
+++ caljs.js
@@ -0,0 +1,14 @@
+ function btn(val)
+ {
+    document.getElementById("screen").value=document.getElementById("screen").value+val;
+ }
+function ClearDisplay()
+{
+    document.getElementById("screen").value=""
+}
+function equal()
+{
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
